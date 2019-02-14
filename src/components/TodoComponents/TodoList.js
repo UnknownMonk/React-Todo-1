9d1d67048754f0b101978dca9a33d7c8db975644
UnknownMonk <smodcartoon@gmail.com>
2019-02-13 19:03:43 -0500

    changed todoList to div

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -5,11 +5,11 @@ import Todo from './Todo';
 
 const TodoList = props => {
   return (
-    <ul>
+    <div>
       {props.todos.map(todo => (
         <Todo todo={todo} key={todo.id} />
       ))}
-    </ul>
+    </div>
   );
 };
 
